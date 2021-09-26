cf3adeb7bdd7cf6263faf66240c14707cfd23ec7
RocketFan <kubal123.tomczak@gmail.com>
2021-09-26 15:41:18 +0200

    Add test structure to project

---
+++ lib/algebra_ext/test/eigen_ext_test.cpp
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(EigenExtTest, Test) {
+    EXPECT_TRUE(true);
+}
---
+++ lib/algebra_ext/test/line_ext_test.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+TEST(LineExtTests, Test) {
+    EXPECT_TRUE(true);
+    EXPECT_TRUE(true);
+}
